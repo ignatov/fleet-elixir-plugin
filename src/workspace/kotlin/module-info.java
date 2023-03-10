fbab20ad720cdeca5d49e1cc887c0891dfc0ad49
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2023-03-10 11:12:02 +0100

    cleanup modules

--- src/workspace/kotlin/module-info.java
+++ src/workspace/kotlin/module-info.java
@@ -1,14 +1,11 @@
 module fleet.elixir.workspace {
-  requires com.google.gson;
   requires fleet.elixir.common;
-  requires fleet.code.protocol;
-  requires fleet.common;
+  requires kotlin.stdlib;
+  requires fleet.util.logging.api;
   requires fleet.kernel;
   requires fleet.lsp;
-  requires fleet.run.common;
+  requires fleet.common;
   requires fleet.workspace;
-  requires org.eclipse.lsp4j.jsonrpc;
-  requires org.eclipse.lsp4j;
 
   exports fleet.elixir.workspace;
 }
